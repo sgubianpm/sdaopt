cf054cc862444e8f2d8a43491ae5e07795006cf9
sgubianpm <sylvain.gubian@pmi.com>
2016-06-27 16:39:50 +0200

    Adjust for running python functions

--- gensabenchmarks/rbench/r_bench_run.py
+++ gensabenchmarks/rbench/r_bench_run.py
@@ -3,56 +3,59 @@ import sys
 import argparse
 import logging
 #from jinja2 import Template, Environment, FileSystemLoader
-#import rpy2.robjects as R
+from collections import OrderedDict
+import rpy2.robjects as R
+from rpy2.robjects.vectors import ListVector
 #from rpy2.robjects.packages import importr
-
-from go_funcs_A import *
-
+from gensabenchmarks.rbench.convert2r import GOClass2RConverter
+from gensabenchmarks.go_func_utils import goclass
 
 logger = logging.getLogger(__name__)
 
 def csv(value):
     return value.split(",")
 
-all_funcs = get_all_func_klass()
-
-def get_all_func_klass():
-    d = {}
-    for name, klass in self.benchmark_functions:
-        d[name] = klass
-
-
 def benchmark(methods, nbruns, deltas, output_dir):
     script_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-            '..', 'scripts')
-    R.r['source'](os.path.join(script_path, 'bench.funcs.R'))
+            'scripts')
     R.r['source'](os.path.join(script_path, 'bench.R'))
-    bench_funs = R.r['get']('bench.funs')
-    for i in range(len(bench_funs)):
-        name_index = bench_funs[i].names.index('name')
-        dim_index = bench_funs[i].names.index('dim')
-        run_index = bench_funs[i].names.index('run')
-        if bench_funs[i][run_index][0]:
-            print('Function: %s dim: %s' % (bench_funs[i][name_index][0],
-                str(bench_funs[i][dim_index][0])))
+    od = OrderedDict()
+    # Creating the R list with converted python go testing functions
+    print('Converting SciPy test functions to R...')
+    counter = 0
+    for name, klass in goclass():
+        try:
+            gocc = GOClass2RConverter(klass)
+            #res = gocc.fun(gocc.xglob)
+            od.update(((name, gocc.rlist),))
+        except Exception:
+            print('Ignoring function: {} that can not be translated...'.format(
+                name))
+            counter += 1
+            continue
+    print('{} functions have been ignored'.format(counter))
+    bench_funs =  ListVector(od)
+    print('Launching R benchmark...')
     RF_benchmark = R.r['bench.run']
     RF_save = R.r['save.res']
     results = {}
     r_deltas = R.FloatVector([float(x) for x in deltas])
     for method in methods:
-        results[method] = RF_benchmark(bench_funs=bench_funs, meth=method,
-                deltas=r_deltas, nb_runs=int(nbruns))
+        results[method] = RF_benchmark(benchfuns=bench_funs, meth=method,
+                deltas=r_deltas, nbruns=int(nbruns))
         RF_save(results[method], filepath=os.path.join(output_dir,
             (method + '.rda')))
+
 def main():
     parser = argparse.ArgumentParser(description='GenSA comparison runner')
     parser.add_argument('--methods', dest='methods', action='store',
-            default="GenSA,DEoptim,DEoptim_BFGS", type=csv,
+            default="GenSA,rgenoud,DEoptim_BFGS", type=csv,
             help='List of method to use in: GenSA, DEoptim, DEoptim_LBFGS')
     parser.add_argument('--nbruns', dest='nbruns', action='store',
-            default=100, help='Number of runs')
+            default=2, help='Number of runs')
     parser.add_argument('--deltas', dest='deltas', action='store',
-            default="1e-5,1e-7,1e-9", type=csv,
+            #default="1e-5,1e-7,1e-9", type=csv,
+            default="1e-8", type=csv,
             help='Delta values comma separated')
     parser.add_argument('--output-dir', dest='output_dir', action='store',
             default=os.path.join(os.getcwd(),'bench_robjects'),
