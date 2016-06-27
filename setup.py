85970687f4bee6f0878ad064f9472da3b7980936
sgubianpm <sylvain.gubian@pmi.com>
2016-06-27 16:40:10 +0200

    Deploy the main

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'numpy',
-    'scipy'
-    'pytest',
+    'scipy',
     'rpy2',
+    'pytest',
 #    'asciitable',
 #    'xlsxwriter',
 #    'sphinx',
@@ -26,23 +26,27 @@ requires = [
 #    'sphinx_bootstrap_theme',
     ]
 
-setup(name='gensabench',
-      version='0.0.1',
-      description='Set of tools for benchmarking GenSA General Simulated Annealing algorithm',
-      long_description=README + '\n\n' +  CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      author='Sylvain Gubian, PMP SA',
-      author_email='sylvain.gubian@pmi.com',
-      url='https://github.com/sgubianpm/gensabench',
-      keywords='optimization benchmarking simulated annealing',
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="bench",
-      entry_points = """\
-      """,
+setup(
+        name='gensabenchmarks',
+        version='0.0.1',
+        description='Set of tools for benchmarking GenSA General Simulated Annealing algorithm',
+        long_description=README + '\n\n' +  CHANGES,
+        classifiers=[
+          "Programming Language :: Python",
+          ],
+        author='Sylvain Gubian, PMP SA',
+        author_email='sylvain.gubian@pmi.com',
+        url='https://github.com/sgubianpm/gensabench',
+        keywords='optimization benchmarking simulated annealing',
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=requires,
+        tests_require=requires,
+        test_suite="bench",
+        entry_points = {
+            'console_scripts': [
+                'rbench = gensabenchmarks.rbench.r_bench_run:main',
+            ],
+        }
       )
 
