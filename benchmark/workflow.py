b265b8d0e8b9d11ba64a48a2a20e4084cd1b773e
sgubian <sylvain.gubian@pmi.com>
2017-02-01 14:10:02 +0100

    Added workflow fore reviewer to execute the benchmark (forgotten)

---
+++ benchmark/workflow.py
@@ -0,0 +1,55 @@
+##############################################################################
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+##############################################################################
+# -*- coding: utf-8 -*-
+import os
+import sys
+import logging
+from benchmark.bench import Benchmarker
+from benchmark.benchstore import BenchStore
+
+__author__ = "Sylvain Gubian"
+__copyright__ = "Copyright 2016, PMP SA"
+__license__ = "GPL2.0"
+__email__ = "Sylvain.Gubian@pmi.com"
+
+logger = logging.getLogger(__name__)
+
+NB_RUNS = 2
+OUTPUT_FOLDER = os.getcwd()
+# If cluser is going to be used uncomment this section and
+# launch the section number to be executed
+# import multiprocessing
+# os.environ['USE_CLUSTER'] = '1'
+# os.environ['SECTION_NUM'] = '0'
+# os.environ['NB_CORES'] = str(multiprocessing.cpu_count())
+
+# For big dimension problems benchmark, this has to be defined
+os.environ['MULTI_DIM'] = '1'
+
+# Default settings will use the available cores on the local machine
+# For high dimension benchmarking, few functions have been selected from
+# the set where functions expression can be generalized for dimension n.
+
+def main():
+    root = logging.getLogger()
+    root.setLevel(logging.WARNING)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    
+    bm = Benchmarker(NB_RUNS, OUTPUT_FOLDER)
+# This may take a long time depending of NB_RUNS and number of cores available
+    bm.run()
+    # Generating the csv report file from the benchmark 
+    path = os.path.join(OUTPUT_FOLDER, 'results.csv')
+    BenchStore.report(
+        kind='csv', path=path, folder=OUTPUT_FOLDER)
+
+if __name__ == '__main__':
+    main()
