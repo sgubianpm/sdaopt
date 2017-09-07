b943cf7e5f04e95e95580e857a76b12e20259f45
Sylvain Gubian <sylvain.gubian@pmi.com>
2017-09-07 22:40:13 +0200

    Adjusted for new implementation

--- hygsa/__init__.py
+++ hygsa/__init__.py
@@ -2,5 +2,13 @@
 # Copyright (c) 2017 Sylvain Gubian <sylvain.gubian@pmi.com>,
 # Yang Xiang <yang.xiang@pmi.com>
 # Author: Sylvain Gubian, PMP S.A.
-from .hygsa import hygsa
-from .hygsa import HyGSARunner
+from __future__ import division, print_function, absolute_import
+
+from ._hygsa import hygsa
+
+# For unit testing
+from ._hygsa import VisitingDistribution
+from ._hygsa import HyGSARunner
+from ._hygsa import ObjectiveFunWrapper
+from ._hygsa import EnergyState
+
